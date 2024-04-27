47704aa4d61cd5514dbdb283c142e674d044706f
Samuel <samuel.montoya@udea.edu.co>
2024-04-27 04:24:55 -0500

    Diseñoas para ambas orientaciones

--- app/src/main/java/co/edu/udea/compumovil/proyectofinal/MainActivity.kt
+++ app/src/main/java/co/edu/udea/compumovil/proyectofinal/MainActivity.kt
@@ -1,11 +1,17 @@
 package co.edu.udea.compumovil.proyectofinal
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +20,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import co.edu.udea.compumovil.proyectofinal.ui.theme.ProyectoFinalTheme
@@ -23,64 +30,144 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ProyectoFinalTheme {
-                    MeimViu()
-                }
+                MeimViu()
             }
         }
     }
+}
 
 
 @Preview(showSystemUi = true)
 @Composable
-fun MeimViu () {
+fun MeimViu() {
+    val configuration = LocalConfiguration.current
+    val isHorizontal = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+
     var puntosA by rememberSaveable {
         mutableIntStateOf(0)
     }
     var puntosB by rememberSaveable {
         mutableIntStateOf(0)
     }
-    var setsA by rememberSaveable {
+    val setsA by rememberSaveable {
         mutableIntStateOf(0)
     }
-    var setsB by rememberSaveable {
+    val setsB by rememberSaveable {
         mutableIntStateOf(0)
     }
-    LazyRow {
-        item {
-            LazyColumn (
-                Modifier
-                    .padding(end = 16.dp))
-                    {
-                item {
-                    Button(onClick = {puntosA++}) {
+
+    if (isHorizontal){
+        LazyRow(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            item {
+                Column(
+                    modifier = Modifier.padding(end = 16.dp)
+                ) {
+                    Button(
+                        onClick = { puntosA++ },
+                        Modifier
+                            .size(200.dp)
+                            .padding(8.dp),
+                        shape = RoundedCornerShape(10.dp),
+                    ) {
                         Text(text = puntosA.toString())
                     }
-                    Button(onClick = {}) {
+                    Button(
+                        onClick = {},
+                        Modifier
+                            .size(100.dp)
+                            .padding(8.dp),
+                        shape = RoundedCornerShape(10.dp),
+                    ) {
                         Text(text = "T")
                     }
                 }
             }
-            LazyColumn {
-                item {
-                    LazyRow {
-                        item {
-                            Text(text = setsA.toString())
-                            Text(text = "-")
-                            Text(text = setsB.toString())
-                        }
+            item {
+                Column {
+                    Text(text = "$setsA - $setsB", Modifier.padding(top = 40.dp, end = 12.dp))
+                }
+            }
+            item {
+                Column(
+                    modifier = Modifier.padding(end = 16.dp)
+                ) {
+                    Button(
+                        onClick = { puntosB++ },
+                        Modifier
+                            .size(200.dp)
+                            .padding(8.dp),
+                        shape = RoundedCornerShape(10.dp),
+                    ) {
+                        Text(text = puntosB.toString())
+                    }
+                    Button(onClick = {},
+                        Modifier
+                            .size(100.dp)
+                            .padding(8.dp),
+                        shape = RoundedCornerShape(10.dp),) {
+                        Text(text = "T")
                     }
                 }
             }
-            LazyColumn {
-                item {
-                    Button(onClick = {puntosB++}) {
+        }
+    } else{
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            item {
+                Row(
+                    modifier = Modifier.padding(end = 16.dp)
+                ) {
+                    Button(
+                        onClick = { puntosA++ },
+                        Modifier
+                            .size(200.dp)
+                            .padding(8.dp),
+                        shape = RoundedCornerShape(10.dp),
+                    ) {
+                        Text(text = puntosA.toString())
+                    }
+                    Button(
+                        onClick = {},
+                        Modifier
+                            .size(100.dp)
+                            .padding(8.dp),
+                        shape = RoundedCornerShape(10.dp),
+                    ) {
+                        Text(text = "T")
+                    }
+                }
+            }
+            item {
+                Column {
+                    Text(text = "$setsA - $setsB", Modifier.padding(top = 40.dp, end = 12.dp))
+                }
+            }
+            item {
+                Row(
+                    modifier = Modifier.padding(end = 16.dp)
+                ) {
+                    Button(
+                        onClick = { puntosB++ },
+                        Modifier
+                            .size(200.dp)
+                            .padding(8.dp),
+                        shape = RoundedCornerShape(10.dp),
+                    ) {
                         Text(text = puntosB.toString())
                     }
-                    Button(onClick = {}) {
+                    Button(onClick = {},
+                        Modifier
+                            .size(100.dp)
+                            .padding(8.dp),
+                        shape = RoundedCornerShape(10.dp),) {
                         Text(text = "T")
                     }
                 }
             }
         }
     }
+
 }
